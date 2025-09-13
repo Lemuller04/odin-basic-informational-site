baf9bd8b1d5ae6f34fe8db67104c3a60f5563d3a
Leonardo <leonardomullerdelima@pm.me>
2025-09-13 09:16:35 -0300

    refactor: better maintainability and portability

--- index.js
+++ index.js
@@ -1,75 +1,45 @@
 import http from "node:http";
 import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
-const getFile = async (fileName) => {
-  let data = null;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function getFile(fileName) {
   try {
-    data = await fs.readFile(
-      `/home/leonardo/repos/odin-basic-informational-site/${fileName}`,
-      { encoding: "utf8" },
-    );
-  } catch (err) {
-    console.error(err);
+    return await fs.readFile(join(__dirname, fileName), "utf8");
+  } catch {
+    return null;
   }
+}
 
-  return data;
+const routes = {
+  "/": "index.html",
+  "/about": "about.html",
+  "/contact-me": "contact.html",
 };
 
-const server = http
-  .createServer((req, res) => {
-    try {
-      if (req.url === "/" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
+const server = http.createServer(async (req, res) => {
+  if (req.method !== "GET") {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    return res.end("Method not allowed");
+  }
 
-        (async () => {
-          const data = await getFile("index.html");
+  const fileName = routes[req.url] || "404.html";
+  const data = await getFile(fileName);
 
-          if (data) {
-            res.end(data.toString());
-          } else {
-            throw new Error("Couldn't get index page");
-          }
-        })();
-      } else if (req.url === "/about" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
+  if (data) {
+    res.writeHead(fileName === "404.html" ? 404 : 200, {
+      "Content-Type": "text/html",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Internal server error");
+  }
+});
 
-        (async () => {
-          const data = await getFile("about.html");
-
-          if (data) {
-            res.end(data.toString());
-          } else {
-            throw new Error("Couldn't get about page");
-          }
-        })();
-      } else if (req.url === "/contact" && req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-
-        (async () => {
-          const data = await getFile("contact.html");
-
-          if (data) {
-            res.end(data.toString());
-          } else {
-            throw new Error("Couldn't get contact page");
-          }
-        })();
-      } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-
-        (async () => {
-          const data = await getFile("404.html");
-
-          if (data) {
-            res.end(data.toString());
-          } else {
-            throw new Error("Couldn't get error page");
-          }
-        })();
-      }
-    } catch (err) {
-      console.error(err);
-      res.end("error" + err);
-    }
-  })
-  .listen(8888);
+server.listen(8888, () =>
+  console.log("Server running at http://localhost:8888"),
+);
